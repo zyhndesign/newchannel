39347356bcaa42bd58d8bca5fc7493344d305446
xavieryuan <xavieryuan@gmail.com>
2018-01-26 11:01:10 +0800

    修复香格里拉项目菜单显示问题

---
+++ public/cn/xianggelila/js/src/config.js
@@ -0,0 +1,38 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: ty
+ * Date: 13-8-22
+ * Time: 下午4:59
+ * config file,contains some static data,these data ara not change by procedure code but artificial change
+ * */
+var ZY=ZY||{};
+
+ZY.config={
+    ajaxurl:"http://www.comdesignlab.com/travel/wp-admin/admin-ajax.php",
+    siteurl:"http://www.comdesignlab.com/travel",
+    categoryIds:{
+        programId:72,  //每个分类的id
+        peopleId:73,
+        landscapeId:74,
+        communityId:76,
+        artifactId:75
+    },
+    articleWidths:{
+        peopleWidth:240, //每个分类的li的宽度
+        landscapeWidth:320,
+        communityWidth:240,
+        artifactWidth:320
+    },
+    errorCode:{
+        connectionError:"无法连接到服务器。", //错误提示信息
+        musicError:"无法从服务器获取音乐。",
+        hasNoMusic:"没有上传音乐",
+        postsError:"无法从服务器获取文章摘要。",
+        articleError:"无法从服务器获取文章详情。"
+    },
+	deviceCode:{
+		iOS : navigator.userAgent.match(/(iPad|iPhone|iPod)/g) ? true : false
+	},
+    defaultWrapZindex:9996 //遮挡层的默认z-index
+};
+Object.freeze(ZY.config);
